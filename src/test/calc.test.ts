c2db4a852e0e5a69bb23fc8c886b2bd5dfd11406
JMSantos77 <1222636@isep.ipp.pt>
2023-09-28 17:13:39 +0100

    Adding test for function sumWithDiscount()

--- src/test/calc.test.ts
+++ src/test/calc.test.ts
@@ -1,7 +1,11 @@
+import { sumOnly, sumWithDiscount } from "../domain/calc";
 
-import { sumOnly } from "../domain/calc";
+test("verifica resultado", () => {
+  let result = sumOnly(1, 2, 3, 4, 5);
+  expect(result).toBe(15);
+});
 
-test("verifica resultado", ()=>{
-    let result = sumOnly(1,2,3,4,5);
-    expect(result).toBe(15);
-})
+test("Verifica desconto", () => {
+  let result = sumWithDiscount(0.3, [1, 2, 3, 4]);
+  expect(result).toBe(7);
+});
