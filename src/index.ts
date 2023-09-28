6de65b2dc2783fc458419998968caa87a47795b9
JMSantos77 <1222636@isep.ipp.pt>
2023-09-28 17:07:17 +0100

    Commit Exercise1

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,10 @@ printMessage(msg);
 
 let list: number[] = [1, 2, 3, 4];
 let total = sumOnly(...list);
-let disc: number=0.3;
-let discountedTotal = sumWithDiscount(disc, list)
+let disc: number = 0.3;
+let discountedTotal = sumWithDiscount(disc, list);
 console.log(total.toString());
 console.log(discountedTotal);
-
 
 // student one: please replace by console.log
 // student two: add to calc.ts a function sumWithDiscount(disc, valores)
