c96c534841fb143d9072ebeb34b4470a5b398d88
Joao Pedro Tavares Medeiros Paiva [1222634] <1222634@isep.ipp.pt>
2023-09-28 16:11:10 +0100

    replaced by console.log

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { sumOnly } from "./domain/calc";
 
-let printMessage = (msg: string): void => console.log(`Mensagem: ${msg}`);
-
 let msg = "Olá Switch-QA";
-printMessage(msg);
+console.log(msg);
 
 let list: number[] = [1, 2, 3, 4];
 let total = sumOnly(...list);
-printMessage(total.toString());
+console.log(total.toString());
 
 // student one: please replace by console.log
 // student two: add to calc.ts a function sumWithDiscount(disc, valores)
